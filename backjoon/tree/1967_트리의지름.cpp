651a81750b9cad07a7103cee0e051f268821c6e6
gomcool17 <gomcool17@naver.com>
2022-05-23 17:50:31 +0900

    2022-05-23

--- backjoon/tree/1967_트리의지름.cpp
+++ backjoon/tree/1967_트리의지름.cpp
@@ -1,4 +1,106 @@
-// 트리의 지름...
-// 루트에서 제일 깊은 곳을 찾기 -> 거기가 한쪽끝
-// 한쪽끝에서 다른 한쪽끝 찾기.... 끝... easy...
-// 내일 풀기
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+vector<pair<int,int>> g[10002];
+int N;
+int arr[10002];
+
+void sol(int now, int pre) {
+
+   for(auto num : g[now]) {
+       int p = num.first;
+       int cost = num.second;
+
+       if(p == pre) continue;
+
+       arr[p] = arr[now] + cost;
+       sol(p, now);
+   }
+
+}
+int main() {
+    cin >> N;
+    for(int i=0;i<N-1;i++) {
+        int a,b,w; cin>>a>>b>>w;
+        g[a].push_back({b,w});
+        g[b].push_back({a,w});
+    }
+    
+    sol(1,0);
+    int m=0;
+    for(int i=1;i<=N;i++) {
+        if(arr[m] < arr[i]) m = i;
+    }
+    memset(arr, 0, sizeof(arr));
+    sol(m,0);
+    int ans = 0;
+
+    for(int i=1;i<=N;i++) ans = max(ans, arr[i]);
+    cout << ans;
+    
+}
+
+/* 다른 버전
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+vector<pair<int,int>> g[10001];
+int N;
+int arr[10001];
+bool visited[10001];
+
+void sol(int parent, int w) {
+
+    queue<pair<int,int>> q;
+    q.push({parent, w});
+    visited[parent] =true;
+    while(!q.empty()) {
+        int p = q.front().first;
+        int ww = q.front().second;
+        q.pop();
+        for(auto num : g[p]) {
+            if(visited[num.first]) continue;
+            visited[num.first] = true;
+            arr[num.first] = arr[p] + num.second;
+            q.push({num.first, num.second});
+        }
+    }
+
+}
+int main() {
+    cin >> N;
+    for(int i=0;i<N-1;i++) {
+        int a,b,w; cin>>a>>b>>w;
+        g[a].push_back({b,w});
+        g[b].push_back({a,w});
+    }
+    
+    sol(1,0);
+    int m=0;
+    for(int i=1;i<=N;i++) {
+        if(arr[m] < arr[i]) m = i;
+    }
+    memset(arr, 0, sizeof(int) * N+1);
+    memset(visited,false,N+1);
+
+    sol(m,0);
+    int ans = 0;
+
+    for(int i=1;i<=N;i++) ans = max(ans, arr[i]);
+    cout << ans << "\n";
+    
+}
+
+
+*/
